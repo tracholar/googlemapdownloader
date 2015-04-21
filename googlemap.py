dad17b49483a0e2ef1c7e70ad54b8c707b7861d5
zuoyuan <zuoyuan@mail.ustc.edu.cn>
2015-04-22 03:38:29 +0800

    remove google

--- googlemap.py
+++ googlemap.py
@@ -1,13 +1,14 @@
 # coding:utf-8
 
-import urllib
+import urllib,urllib2
 import numpy as np
 import threading, os
 from maps import *
+import time
 
 imsize = 512
 zoom = 14
-url = r'https://maps.googleapis.com/maps/api/staticmap?center={center}&zoom=%d&size=%dx%d&maptype=satellite&sensor=false' % (zoom,imsize,imsize)
+url = r'https://maps.googleapis.com/maps/api/staticmap?center={center}&zoom=%d&size=%dx%d&maptype=roadmap&sensor=false' % (zoom,imsize,imsize)
 
 center = [40.143957,94.6297456]
 
@@ -15,7 +16,7 @@ center = [40.143957,94.6297456]
 L_long = 94.55
 R_long = 94.95
 T_lat = 40.4
-B_lat = 40.1
+B_lat = 40.0
 
 L_x = Long2Pixel(L_long, zoom)
 R_x = Long2Pixel(R_long, zoom)
@@ -23,6 +24,10 @@ R_x = Long2Pixel(R_long, zoom)
 T_y = Lat2Pixel(T_lat, zoom)
 B_y = Lat2Pixel(B_lat, zoom)
 
+dx = imsize
+x_range = range(L_x,R_x+imsize,dx)
+dy = imsize - 35
+y_range = range(T_y, B_y + imsize, dy)
 pos = []
 
 def DownloadMap():
@@ -45,12 +50,13 @@ def DownloadMap():
 		f.write(img)
 		f.close()
 		print lat,long
+		time.sleep(10)
 	
 if __name__ == '__main__':
 	
 	
-	for x in range(L_x,R_x+imsize,imsize):
-		for y in range(T_y, B_y + imsize, imsize):
+	for x in x_range:
+		for y in y_range:
 			long, lat = Pixel2Long(x, zoom), Pixel2Lat(y, zoom)
 			pos.append((long,lat))
 	
--- imgmerge.py
+++ imgmerge.py
@@ -4,22 +4,22 @@ from PIL import Image
 import numpy as np
 from googlemap import *
 
-n_X = len(range(L_x,R_x+imsize,imsize))
-n_Y = len(range(T_y, B_y + imsize, imsize))
+n_X = len(x_range)
+n_Y = len(y_range)
 
-im_size = (n_X * imsize,n_Y * imsize)
+im_size = (n_X * dx,n_Y * dy)
 im = Image.new("RGB",im_size)
 
-for x in range(L_x,R_x+imsize,imsize):
-	for y in range(T_y, B_y + imsize, imsize):
+for x in x_range:
+	for y in y_range:
 		long, lat = Pixel2Long(x, zoom), Pixel2Lat(y, zoom)
 		im_name = 'map_%f_%f' % (lat,long)
 		im_name = im_name.replace('.','') + '.png'
 		
 		I = Image.open(im_name)
 		
-		L = int((x - L_x)/imsize)
-		T = int((y - T_y)/imsize) 
-		im.paste(I,(L*imsize,T*imsize))
+		L = int((x - L_x)/dx)
+		T = int((y - T_y)/dy) 
+		im.paste(I,(L*dx,T*dy))
 		
 im.save('bigimg.bmp')
